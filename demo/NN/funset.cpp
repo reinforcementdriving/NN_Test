0d1c738b8b5e9371840f80e106bb1cb018c025b8
fengbingchun <fengbingchun@163.com>
2018-02-22 13:35:56 +0800

    Add Logistic Regression C++ code implement

--- demo/NN/funset.cpp
+++ demo/NN/funset.cpp
@@ -11,6 +11,111 @@
 #include "decision_tree.hpp"
 #include "pca.hpp"
 #include <opencv2/opencv.hpp>
+#include "logistic_regression2.hpp"
+
+// ================================ logistic regression =====================
+int test_logistic_regression2_train()
+{
+	const std::string image_path{ "E:/GitCode/NN_Test/data/images/digit/handwriting_0_and_1/" };
+	cv::Mat data, labels;
+
+	for (int i = 1; i < 11; ++i) {
+		const std::vector<std::string> label{ "0_", "1_" };
+
+		for (const auto& value : label) {
+			std::string name = std::to_string(i);
+			name = image_path + value + name + ".jpg";
+
+			cv::Mat image = cv::imread(name, 0);
+			if (image.empty()) {
+				fprintf(stderr, "read image fail: %s\n", name.c_str());
+				return -1;
+			}
+
+			data.push_back(image.reshape(0, 1));
+		}
+	}
+	data.convertTo(data, CV_32F);
+
+	std::unique_ptr<float[]> tmp(new float[20]);
+	for (int i = 0; i < 20; ++i) {
+		if (i % 2 == 0) tmp[i] = 0.f;
+		else tmp[i] = 1.f;
+	}
+	labels = cv::Mat(20, 1, CV_32FC1, tmp.get());
+
+	ANN::LogisticRegression2<float> lr;
+	const float learning_rate{ 0.00001f };
+	const int iterations{ 1000 };
+	int ret = lr.init((float*)data.data, (float*)labels.data, data.rows, data.cols);
+	if (ret != 0) {
+		fprintf(stderr, "logistic regression init fail: %d\n", ret);
+		return -1;
+	}
+
+	const std::string model{ "E:/GitCode/NN_Test/data/logistic_regression2.model" };
+
+	ret = lr.train(model);
+	if (ret != 0) {
+		fprintf(stderr, "logistic regression train fail: %d\n", ret);
+		return -1;
+	}
+
+	return 0;
+}
+
+int test_logistic_regression2_predict()
+{
+	const std::string image_path{ "E:/GitCode/NN_Test/data/images/digit/handwriting_0_and_1/" };
+	cv::Mat data, labels, result;
+
+	for (int i = 11; i < 21; ++i) {
+		const std::vector<std::string> label{ "0_", "1_" };
+
+		for (const auto& value : label) {
+			std::string name = std::to_string(i);
+			name = image_path + value + name + ".jpg";
+
+			cv::Mat image = cv::imread(name, 0);
+			if (image.empty()) {
+				fprintf(stderr, "read image fail: %s\n", name.c_str());
+				return -1;
+			}
+
+			data.push_back(image.reshape(0, 1));
+		}
+	}
+	data.convertTo(data, CV_32F);
+
+	std::unique_ptr<int[]> tmp(new int[20]);
+	for (int i = 0; i < 20; ++i) {
+		if (i % 2 == 0) tmp[i] = 0;
+		else tmp[i] = 1;
+	}
+	labels = cv::Mat(20, 1, CV_32SC1, tmp.get());
+
+	CHECK(data.rows == labels.rows);
+
+	const std::string model{ "E:/GitCode/NN_Test/data/logistic_regression2.model" };
+
+	ANN::LogisticRegression2<float> lr;
+	int ret = lr.load_model(model);
+	if (ret != 0) {
+		fprintf(stderr, "load logistic regression model fail: %d\n", ret);
+		return -1;
+	}
+
+	for (int i = 0; i < data.rows; ++i) {
+		float probability = lr.predict((float*)(data.row(i).data), data.cols);
+
+		fprintf(stdout, "probability: %.6f, ", probability);
+		if (probability > 0.5) fprintf(stdout, "predict result: 1, ");
+		else fprintf(stdout, "predict result: 0, ");
+		fprintf(stdout, "actual result: %d\n", ((int*)(labels.row(i).data))[0]);
+	}
+
+	return 0;
+}
 
 // =============================== PCA(Principal Components Analysis) ===================
 namespace {
--- demo/NN/funset.hpp
+++ demo/NN/funset.hpp
@@ -1,27 +1,21 @@
 #ifndef FBC_TEST_NN_FUNSET_HPP_
 #define FBC_TEST_NN_FUNSET_HPP_
 
+int test_logistic_regression2_train();
+int test_logistic_regression2_predict();
 int test_pca();
-
 int test_decision_tree_train();
 int test_decision_tree_predict();
-
 int test_knn_classifier_predict();
-
 int test_logistic_regression_train();
 int test_logistic_regression_predict();
-
 int test_perceptron();
-
 int test_BP_train();
 int test_BP_predict();
-
 int test_CNN_train();
 int test_CNN_predict();
-
 int test_linear_regression_train();
 int test_linear_regression_predict();
-
 int test_naive_bayes_classifier_train();
 int test_naive_bayes_classifier_predict();
 
